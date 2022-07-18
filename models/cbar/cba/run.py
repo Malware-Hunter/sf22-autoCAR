50648b6a83bcf99f4ea5b89351654c9194a7efdb
vanderson-rocha <vanderson.rocha@gmail.com>
2022-07-18 11:03:37 -0400

    Update CBA run.py

--- models/cbar/cba/run.py
+++ models/cbar/cba/run.py
@@ -7,17 +7,24 @@ import sys
 import os
 from spinner import Spinner
 from models.utils import *
+from termcolor import colored, cprint
+import logging
 
 def exec_cba(path_to_r_file, train, test, s, c, l):
-    r = ro.r
-    r.source(path_to_r_file)
-    with localconverter(ro.default_converter + pandas2ri.converter):
-        df_train = ro.conversion.py2rpy(train)
-    with localconverter(ro.default_converter + pandas2ri.converter):
-        df_test = ro.conversion.py2rpy(test)
+    l = []
+    try:
+        r = ro.r
+        r.source(path_to_r_file)
+        with localconverter(ro.default_converter + pandas2ri.converter):
+            df_train = ro.conversion.py2rpy(train)
+        with localconverter(ro.default_converter + pandas2ri.converter):
+            df_test = ro.conversion.py2rpy(test)
 
-    p = r.cba(df_train, df_test, s, c, l)
-    l = [x for x in p]
+        p = r.cba(df_train, df_test, s, c, l)
+        l = [x for x in p]
+    except BaseException as e:
+        msg = colored('Exception When Running CBAR CBA.\n{}'.format(e), 'red')
+        logging.error(msg)
     return l
 
 #if __name__=="__main__":
@@ -38,8 +45,9 @@ def run(dataset, dataset_file, args):
         spn.start()
         prediction_result = exec_cba(path_to_r_file, train, test, args.min_support, args.min_confidence, args.max_length)
         spn.stop()
-        general_class += list(test['class'])
-        general_prediction += prediction_result
+        if prediction_result:
+            general_class += list(test['class'])
+            general_prediction += prediction_result
         fold_no += 1
 
     return general_class, general_prediction
