76cf8e897685edcf46ddb80989acbefb61edc203
Diego Kreutz <diego.unipampa@gmail.com>
2022-08-08 09:15:17 -0300

    updates

--- docker_setup.sh
+++ docker_setup.sh
@@ -6,7 +6,7 @@ DEBIAN_FRONTEND=noninteractive wget -qO- https://cloud.r-project.org/bin/linux/u
 DEBIAN_FRONTEND=noninteractive add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 DEBIAN_FRONTEND=noninteractive apt update
 DEBIAN_FRONTEND=noninteractive apt install --no-install-recommends r-base -y
-DEBIAN_FRONTEND=noninteractive apt install python3-pip -y
+DEBIAN_FRONTEND=noninteractive apt install python3-pip lib64gfortran-10-dev libopenblas64-dev liblapack-pic -y
 DEBIAN_FRONTEND=noninteractive pip install -r requirements.txt
 R --no-save <<SHAR_EOF
 install.packages('arulesCBA')
